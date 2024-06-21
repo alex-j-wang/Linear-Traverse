b25cca0c29c7dd9fced7ebc2d4ca96fa755fb341
alex-j-wang <alexander_wang@brown.edu>
2024-06-21 11:50:34 -0400

    Bug fixes & commenting for sensorless runs

--- dynamic_operation.m
+++ dynamic_operation.m
@@ -4,37 +4,36 @@ function [time, forces, motor_position] = dynamic_operation(CF, F, A, data_cyc, 
     % -----------------------------------------------------------------------
     
     % EXPERIMENT EXECUTION
-    disp("Zeroing output...");
+    SRATE = daq_obj.Rate;
+    disp("Zeroing output");
     tare_output = zeros(offset_dur * SRATE, 1);
     tare_inputs = readwrite(daq_obj, tare_output, "OutputFormat", "Matrix");
 
     % Calculate channel biases
     tare_voltages = mean(tare_inputs);
 
-    disp("Starting Crazyflie...");
-    pause(1)
-    system("ssh anoop@138.16.161.135 ./throttle.sh " + CF);
-    pause(3)
+    % disp("Starting Crazyflie...");
+    % pause(1)
+    % system("ssh anoop@138.16.161.135 ./throttle.sh " + CF);
+    % pause(3)
     
-    disp("Generating voltage profile...");
+    disp("Generating voltage profile");
     position = generate_profile(data_cyc, F, SRATE, ramp_cyc, A);
     data_output = DTOV * position;
 
     tic;
-    disp("Collecting data...");
-    [data_inputs, time, ~] = readwrite(daq_obj, data_output, "OutputFormat", "Matrix");
+    disp("Collecting data");
+    [data_inputs, time, ~] = readwrite(daq_obj, data_output', "OutputFormat", "Matrix");
     toc;
     
-    system("ssh anoop@138.16.161.135 ./throttle.sh 0");
+    % system("ssh anoop@138.16.161.135 ./throttle.sh 0");
 
     % DATA EXTRACTION
-    disp("Extracting data...");
-    data_voltages = data_inputs(RAMP_CYCLES*SRATE : end-RAMP_CYCLES*SRATE, :);
+    disp("Extracting data");
+    data_voltages = data_inputs(ramp_cyc*SRATE : end-ramp_cyc*SRATE, :);
     motor_position = data_voltages(:, 7) / DTOV;
     sensor_voltages = data_voltages(:, 1:6) - tare_voltages(:, 1:6);
     forces = (cal_mat * sensor_voltages')'; % Conversion to forces and moments
-
-    disp("Data collection complete.");
 end
 
 function position = generate_profile(data_cycles, traverse_freq, sampling_freq, ramp_cycles, amplitude)
@@ -52,6 +51,6 @@ function position = generate_profile(data_cycles, traverse_freq, sampling_freq, 
     % Modulate ends using sinusoidal multiplier
     pts_ramp = round(pts_per_cycle * ramp_cycles); % Number of points to be modulated on either end
     multiplier = 0.5 * (1 - cos(pi * (0 : 1/pts_ramp : 1)));
-    position(1 : pts_ramp) = position(1 : pts_ramp) .* multiplier;
-    position(end-pts_ramp+1 : end) = position(end-pts_ramp+1 : end) .* fliplr(multiplier);
+    position(1 : pts_ramp+1) = position(1 : pts_ramp+1) .* multiplier;
+    position(end-pts_ramp : end) = position(end-pts_ramp : end) .* fliplr(multiplier);
 end
--- execution.m
+++ execution.m
@@ -1,24 +1,21 @@
 clear; clc; close all;
 
-% Set the default datetime display format
-setpref('datetime', 'DefaultFormat', 'hh:mm:ss');
-
 % Constant parameters
 DATA_CYCLES = 40; % Cycles of data for phase averaging
 RAMP_CYCLES = 4; % Cycles for ramping up and down
 OFFSET_DURATION = 10; % Duration for zeroing force transducer, s
 
 % Test parameters
-CFS = [25 50 75]; % Crazyflie throttle, %
-SDS = [0.5 1 2 5 10] / 100; % Stopping distance, m
-FS = 0.3:0.1:1; % Traverse frequency, Hz
+CFS = 75; % Crazyflie throttle, %
+SDS = 0.5 / 100; % Stopping distance, m
+FS = 0.3:0.3:1; % Traverse frequency, Hz
 AS = 0.025:0.025:0.1; % Traverse amplitude, m
 
 % DAQ setup
 SRATE = 20000; % Data sampling rate, Hz
 DTOV = 1 / .02; % Conversion factor from distance to voltage, V/m
 
-disp("Setting up DAQ...");
+disp("Setting up DAQ");
 daq_obj = daq("ni");
 daq_obj.Rate = SRATE;
 
@@ -51,33 +48,35 @@ names = ["F_x" "F_y" "F_z" "M_x" "M_y" "M_z"];
 % Wait for DAQ setup to stabilize
 pause(1);
 
-disp("Calibrating stopping distance. Press ENTER to begin...");
-pause;
+% disp("Calibrating stopping distance. Press ENTER to begin...");
+% pause;
 
-CAL_SPEED = 0.05; % m/s
-END = 0.2; % m
-prev = read(daq_obj);
-start(daq_obj);
-cal_output = DTOV * (prev.MotorPosition : CAL_SPEED / SRATE : END);
-write(daq_obj, cal_output');
+% CAL_SPEED = 0.05; % m/s
+% END = 0.2; % m
+% prev = read(daq_obj);
+% start(daq_obj);
+% cal_output = DTOV * (prev.MotorPosition : CAL_SPEED / SRATE : END);
+% write(daq_obj, cal_output');
 
-while true
-    pause(0.1);
-    curr = read(daq_obj);
-    if abs(curr.MotorPosition - prev.MotorPosition) / DTOV < 0.001
-        break;
-    end
-    prev = curr;
-end
-
-stop(daq_obj);
-ground = curr(7) - input("Enter distance from ground plane (cm): ") / 100;
+% while true
+%     pause(0.1);
+%     curr = read(daq_obj);
+%     if abs(curr.MotorPosition - prev.MotorPosition) / DTOV < 0.001
+%         break;
+%     end
+%     prev = curr;
+% end
+% 
+% stop(daq_obj);
+% ground = curr(7) - input("Enter distance from ground plane (cm): ") / 100;
 
 est_time = seconds(length(CFS) * length(SDS) * length(AS) * ...
-    ((DATA_CYCLES + 2 * RAMP_CYCLES) * sum(1 / FS) + OFFSET_DURATION * length(FS)));
+    ((DATA_CYCLES + 2 * RAMP_CYCLES) * sum(1 ./ FS) + OFFSET_DURATION * length(FS)));
+est_time.Format = 'hh:mm:ss';
 est_elapsed = seconds(0);
+est_elapsed.Format = 'hh:mm:ss';
 
-h = waitbar(0, "Initializing...");
+h = waitbar(0, "Initializing...", "Name", "Dynamic Testing");
 tic
 
 % ACQUIRE DATA
@@ -86,23 +85,23 @@ for CF = CFS
         for F = FS
             for A = AS
                 case_name = sprintf("CF%d_SD%.1f_F%.1f_A%.1f", CF, SD * 100, F, A * 100);
-                disp(case_name);
+                disp("Running " + case_name);
 
-                actual_elapsed = seconds(toc);  
-                message = sprintf("Current case: %s\nEstimated execution time: %s\nElapsed time: %s", ...
-                    case_name, est_time, actual_elapsed);
+                actual_elapsed = seconds(toc);
+                actual_elapsed.Format = 'hh:mm:ss';
+                message = sprintf("Estimated execution time: %s\nElapsed time: %s", est_time, actual_elapsed);
                 waitbar(est_elapsed / est_time, h, message);
                 
                 % Move to starting position
-                target = ground + A + SD;
-                disp(['Moving to ' (num2str(target) * 100) ' cm...']);
-                pause(1);
-                write(daq_obj, target * DTOV);
-                pause(3);
+                % target = ground + A + SD;
+                % disp(['Moving to ' (num2str(target) * 100) ' cm...']);
+                % pause(1);
+                % write(daq_obj, target * DTOV);
+                % pause(3);
 
                 % Gather data
                 [time, forces, motor_position] = ...
-                    dynamic_operation_manual(CF, F, A, DATA_CYCLES, RAMP_CYCLES, OFFSET_DURATION, DTOV, daq_obj, cal_mat);
+                    dynamic_operation(CF, F, A, DATA_CYCLES, RAMP_CYCLES, OFFSET_DURATION, DTOV, daq_obj, cal_mat);
                 
                 % Save data
                 filename = fullfile(date_string, case_name + '.mat');
@@ -118,7 +117,7 @@ for CF = CFS
                     fprintf("%s: %.2f (%.2f)\n", names(i), mean_forces(i), std_forces(i));
                 end
 
-                est_elapsed = est_elapsed + seconds(DATA_CYCLES + 2 * RAMP_CYCLES) * (1 / F) + OFFSET_DURATION;
+                est_elapsed = est_elapsed + seconds((DATA_CYCLES + 2 * RAMP_CYCLES) * (1 / F) + OFFSET_DURATION);
             end
         end
     end
