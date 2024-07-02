1897964e03684d6e78e0d56ef461372ff5b0b60e
Alex Wang <alexander_wang@brown.edu>
2024-07-02 13:43:39 -0400

    Manual limit processing & cleanup

--- limit_processing.m
+++ limit_processing.m
@@ -1,11 +1,8 @@
 % -----------------------------------------------------------------------
-% Data Analysis Code for Dynamic Quadrotor Experiments
+% Error Analysis Code for Dynamic Quadrotor Experiments
 % -----------------------------------------------------------------------
 
 clear; clc; close all hidden;
-
-SRATE = 20000; % Sampling frequency
-FC = 20; % Cut-off frequency
 
 folder = "Limit Analysis";
 items = dir(fullfile(folder, "*.mat"));
@@ -13,17 +10,13 @@ filenames = sort({items.name});
 
 pattern = "F%f_A%f.mat";
 
-% Create a uifigure
+% Create progess bar
 fig = uifigure('Name', 'Dynamic Processing');
-
-% Create the uiprogressdlg
 d = uiprogressdlg(fig, 'Title', 'Processing', 'Message', 'Initializing...', 'Indeterminate', 'on');
-
-pause(1); % Simulate some initialization time
-
+pause(1);
 d.Indeterminate = 'off';
 
-% Define the column names
+% Data table
 columns = {'IntendedAmplitude', 'TargetAmplitude', 'MeasuredAmplitude', ...
     'IntendedFrequency', 'TargetFrequency', 'MeasuredFrequency', ...
     'IntendedPhase', 'TargetPhase', 'MeasuredPhase'
@@ -41,6 +34,7 @@ for i = 1 : length(filenames)
 
     load(fullfile(folder, filename));
 
+    % Fit sinusoids and record results
     target = fit_sinusoid(time, position, A, F);
     measured = fit_sinusoid(time, motor_position, A, F);
     
@@ -55,6 +49,23 @@ for i = 1 : length(filenames)
     data.IntendedPhase(i) = 0;
     data.TargetPhase(i) = target.C;
     data.MeasuredPhase(i) = measured.C;
+
+    % Plot an example as confirmation
+    if i == 1
+        figure
+        p_title = sprintf('Example Sinusoid Fit (A = %g cm, F = %g Hz)', A * 100, F);
+        formatplot(p_title, 'Time (s)', 'Position (cm)');
+        plot(time, 100 * motor_position);
+        plot(time, 100 * measured(time));
+        legend('Measured', 'Fit');
+
+        figure
+        p_title = sprintf('Example Wave Comparison (A = %g cm, F = %g Hz)', A * 100, F);
+        formatplot(p_title, 'Time (s)', 'Position (cm)');
+        plot(time, 100 * target(time));
+        plot(time, 100 * measured(time));
+        legend('Target', 'Measured');
+    end
 end
 
 disp('All files processed');
@@ -63,6 +74,7 @@ close(fig);
 data = sortrows(data, ["IntendedAmplitude", "IntendedFrequency"]);
 writetable(data, fullfile(folder, 'results.csv'));
 
+% Create Bode plots
 AS = unique(data.IntendedAmplitude);
 t = tiledlayout(length(AS), 2, 'TileSpacing', 'compact', 'Padding', 'compact');
 title(t, 'Linear Traverse Error Versus Frequency');
@@ -81,20 +93,16 @@ for A = AS'
     plot(selection.IntendedFrequency, 100 * abs(selection.IntendedAmplitude - selection.MeasuredAmplitude), ".-");
 end
 
-function fitresult = fit_sinusoid(x, y, A0, B0)
-    % Ensure that x and y are column vectors
+function fitresult = fit_sinusoid(x, y, A, F)
+    % Convert to column vectors
     x = x(:);
     y = y(:);
     
     % Define the sinusoidal fit type
     ft = fittype('A*sin(2*pi*B*x + C) + D', 'independent', 'x', 'coefficients', {'A', 'B', 'C', 'D'});
-    
-    % Initial guesses for the coefficients
-    C0 = 0; % Phase estimate
-    D0 = mean(y); % Vertical shift estimate
-    
+
     % Fit the model to the data
-    fitresult = fit(x, y, ft, 'StartPoint', [A0, B0, C0, D0]);
+    fitresult = fit(x, y, ft, 'StartPoint', [A, F, 0, 0]);
 end
 
 function formatplot(p_title, p_x, p_y)
---
+++ limit_processing_manual.m
@@ -0,0 +1,83 @@
+% -----------------------------------------------------------------------
+% Error Analysis Code for Dynamic Quadrotor Experiments
+% -----------------------------------------------------------------------
+
+clear; clc; close all hidden;
+
+SRATE = 20000;
+
+folder = "Limit Analysis";
+items = dir(fullfile(folder, "*.mat"));
+filenames = sort({items.name});
+
+pattern = "F%f_A%f.mat";
+
+% Create progess bar
+fig = uifigure('Name', 'Dynamic Processing');
+d = uiprogressdlg(fig, 'Title', 'Processing', 'Message', 'Initializing...', 'Indeterminate', 'on');
+pause(1);
+d.Indeterminate = 'off';
+
+% Data table
+columns = {'TargetAmplitude', 'MeasuredAmplitude', ...
+    'TargetFrequency', 'MeasuredFrequency', ...
+    'TargetPhase', 'MeasuredPhase'
+};
+data = array2table(NaN(length(filenames), length(columns)), 'VariableNames', columns);
+
+for i = 1 : length(filenames)
+    filename = filenames{i};
+    d.Value = (i - 1) / length(filenames);
+    d.Message = strrep(filename, '_', ' ');
+
+    parameters = num2cell(sscanf(filename, pattern));
+    [F, A] = deal(parameters{:});
+    A = A / 100;
+
+    load(fullfile(folder, filename));
+    
+    data.TargetAmplitude(i) = A;
+    data.TargetFrequency(i) = F;
+    data.TargetPhase(i) = 0;
+
+    data.MeasuredAmplitude(i) = range(motor_position) / 2;
+
+    integral_product = trapz(time, position .* motor_position);
+    magnitude_target = trapz(time, position .^ 2);
+    magnitude_measured = trapz(time, motor_position .^ 2);
+    cos_phi = integral_product / sqrt(magnitude_target * magnitude_measured);
+    data.MeasuredPhase(i) = acos(cos_phi);
+end
+
+disp('All files processed');
+close(fig);
+
+data = sortrows(data, ["TargetAmplitude", "TargetFrequency"]);
+writetable(data, fullfile(folder, 'results_manual.csv'));
+
+% Create Bode plots
+AS = unique(data.TargetAmplitude);
+t = tiledlayout(length(AS), 2, 'TileSpacing', 'compact', 'Padding', 'compact');
+title(t, 'Linear Traverse Error Versus Frequency');
+
+for A = AS'
+    selection = data(data.TargetAmplitude == A, :);
+
+    nexttile
+    p_title = sprintf("Phase Lag Versus Input Frequency (A = %g cm)", A * 100);
+    formatplot(p_title, "Input Frequency (Hz)", "Phase Lag (rad)");
+    plot(selection.TargetFrequency, abs(selection.TargetPhase - selection.MeasuredPhase), ".-");
+
+    nexttile
+    p_title = sprintf("Amplitude Difference Versus Input Frequency (A = %g cm)", A * 100);
+    formatplot(p_title, "Input Frequency (Hz)", "Amplitude Difference (cm)");
+    plot(selection.TargetFrequency, 100 * abs(selection.TargetAmplitude - selection.MeasuredAmplitude), ".-");
+end
+
+function formatplot(p_title, p_x, p_y)
+    title(p_title);
+    xlabel(p_x);
+    ylabel(p_y);
+    hold on
+    grid on
+end
