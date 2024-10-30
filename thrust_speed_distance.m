ee9f2d177bf0007ae521235ffc43b7f7194ca649
alex-j-wang <alexander_wang@brown.edu>
2024-10-30 16:21:06 -0400

    3D plotting use diff

--- thrust_speed_distance.m
+++ thrust_speed_distance.m
@@ -1,7 +1,7 @@
 clear; clc; close all hidden;
 
 folder_path = "Data/2024_10_25_3D/processed_data";
-incr = 500;
+incr = 250;
 
 items = dir(fullfile(folder_path, '*.mat'));
 filenames = string({items.name});
@@ -20,42 +20,15 @@ for filename = filenames
     A = A / 100;
 
     if A == 0
-        distance = SD;
-        velocity = 0;
-        forces = mean(forces.Total(:, 3)) / Config.W;
+        scatter3(SD, 0, mean(forces.Total(:, 3)) / Config.W, 10, 'red', 'filled');
     else
-        position_fit = fit_sinusoid(time, pos_encoder, A, F);
-        if abs(position_fit.D) > 0.01
+        if abs(mean(pos_encoder)) > 0.01
             continue
         end
-        distance = SD - position_fit.D + position_fit.A + position_fit(time);
-        velocity = differentiate(position_fit, time);
-        forces = forces.Total(:, 3) / Config.W;
-    end
-    
-    % if A == 0
-    %     scatter3(distance(1:incr:end), velocity(1:incr:end), forces(1:incr:end), 10, 'red', 'filled');
-    % else
-    %     h = scatter3(distance(1:incr:end), velocity(1:incr:end), forces(1:incr:end), 10, [0 0 sqrt(SD/0.1)], 'filled');
-    %     set(h, 'MarkerEdgeAlpha', 0.3, 'MarkerFaceAlpha', 0.3);
-    % end
-
-    if A == 0
-        scatter3(distance, 0, forces, 10, 'red', 'filled');
-    else
-        h = scatter3(SD + A + pos_encoder(1:incr:end - incr), diff(pos_encoder(1:incr:end)) / incr, forces(1:incr:end - incr), 10, [0 0 sqrt(SD/0.1)], 'filled');
+        distance = SD + A + pos_encoder(1:incr:end - incr);
+        velocity = diff(pos_encoder(1:incr:end)) / incr;
+        forces = forces.Total(1:incr:end - incr, 3) / Config.W;
+        h = scatter3(distance, velocity, forces, 10, [0 0 SD/0.07], 'filled');
         set(h, 'MarkerEdgeAlpha', 0.3, 'MarkerFaceAlpha', 0.3);
     end
-end
-
-function fitresult = fit_sinusoid(t, s, A, F)
-    % Convert to column vectors
-    t = t(:);
-    s = s(:);
-    
-    % Define the sinusoidal fit type
-    ft = fittype('A*sin(2*pi*B*t + C) + D', 'independent', 't', 'coefficients', {'A', 'B', 'C', 'D'});
-
-    % Fit the model to the data
-    fitresult = fit(t, s, ft, 'StartPoint', [A, F, 0, 0]);
 end
