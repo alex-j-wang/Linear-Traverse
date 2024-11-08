424e6d430cd412bec6d5cbd7df9a7b26c285e7b4
Alex Wang <alexander_wang@brown.edu>
2024-11-08 01:02:03 -0500

    Improvements & new plot

--- threedtest.m
+++ threedtest.m
@@ -1,14 +1,17 @@
 clear; clc; close all hidden;
 
 folder_path = "Data/2024_10_25_3D/processed_data";
-incr = 25;
+incr = 100;
+buf = 300;
+MAX = 0.643476;
 
 items = dir(fullfile(folder_path, '*.mat'));
 filenames = string({items.name});
+highlight = ["CF54.275_SD3_F0.2_A7.mat", "CF54.275_SD3_F0.5_A7.mat", "CF54.275_SD3_F1_A7.mat"];
 
 title("Crazyflie Thrust Versus Distance and Velocity");
 xlabel("Distance (m)");
-ylabel("Velocity (m)");
+ylabel("Velocity (m/s)");
 zlabel("Normalized Thrust");
 view(3)
 hold on;
@@ -22,7 +25,10 @@ figure
 Process.format_plot("Phase Averaged Position Versus Time (Shifted)", "Time (s)", "Position (m)");
 shifted = gca;
 
-for filename = filenames
+labels = strings(1, length(filenames));
+
+for i = 1:length(filenames)
+    filename = filenames(i);
     load(fullfile(folder_path, filename), 'time', 'forces', 'pos_encoder');
     parameters = num2cell(sscanf(filename, 'CF%f_SD%f_F%f_A%f.mat'));
     [CF, SD, F, A] = deal(parameters{:});
@@ -35,7 +41,7 @@ for filename = filenames
     plot(shifted, time, pos_encoder);
 
     if A == 0
-        scatter3(main, SD, 0, mean(forces.Total(:, 3)) / Config.W, 10, 'red', 'filled');
+        scatter3(main, SD, 0, mean(forces.Total(:, 3)) / Config.W / MAX, 10, 'red', 'filled');
     else
         position_fit = fit_sinusoid(time, pos_encoder, A, F);
         distance = SD + position_fit.A + position_fit(time);
@@ -46,7 +52,7 @@ for filename = filenames
         them later
         %}
         pad_length = 50; %# points to pad
-        padded_forces = [forces.Total(:, 3) / Config.W; repmat(forces.Total(end, 3) / Config.W, pad_length, 1)];
+        padded_forces = [forces.Total(:, 3) / Config.W / MAX; repmat(forces.Total(end, 3) / Config.W / MAX, pad_length, 1)];
         
         %wavelet denoising
         forces_smoothed_padded = wdenoise(padded_forces, 'NoiseEstimate', 'LevelDependent');
@@ -54,19 +60,26 @@ for filename = filenames
         % removing earlier padded points here 
         forces_smoothed = forces_smoothed_padded(1:end-pad_length);
         forces_smoothed(end-50:end) = movmean(forces_smoothed(end-50:end), 10);
-        h = scatter3(main, distance(1:incr:end), velocity(1:incr:end), forces_smoothed(1:incr:end), 10, 'blue', 'filled');
-        set(h, 'MarkerEdgeAlpha', 0.1, 'MarkerFaceAlpha', 0.1);
+        if ismember(filename, highlight)
+            scatter3(main, distance(buf:incr:end-buf), velocity(buf:incr:end-buf), forces_smoothed(buf:incr:end-buf), 10, 'filled');
+            labels(i) = sprintf("F = %g", F);
+        else
+            h = scatter3(main, distance(buf:incr:end-buf), velocity(buf:incr:end-buf), forces_smoothed(buf:incr:end-buf), 10, 'blue', 'filled');
+            set(h, 'MarkerEdgeAlpha', 0.02, 'MarkerFaceAlpha', 0.02);
+        end
     end
 end
 
+legend(main, labels);
+
 function fitresult = fit_sinusoid(t, s, A, F)
-% Convert to column vectors
-t = t(:);
-s = s(:);
+    % Convert to column vectors
+    t = t(:);
+    s = s(:);
 
-% Define the sinusoidal fit type
-ft = fittype('A*sin(2*pi*B*t + C)', 'independent', 't', 'coefficients', {'A', 'B', 'C'});
+    % Define the sinusoidal fit type
+    ft = fittype('A*sin(2*pi*B*t + C)', 'independent', 't', 'coefficients', {'A', 'B', 'C'});
 
-% Fit the model to the data
-fitresult = fit(t, s, ft, 'StartPoint', [A, F, 0]);
+    % Fit the model to the data
+    fitresult = fit(t, s, ft, 'StartPoint', [A, F, 0]);
 end
--- twodtest_fgrad.m
+++ twodtest_fgrad.m
@@ -1,5 +1,5 @@
 % -------------------------------------------------------------------------
-% Plots thrust versus distance with color to represent speed
+% Plots speed versus distance with color to represent thrust
 % -------------------------------------------------------------------------
 
 %% Dynamic
@@ -13,7 +13,7 @@ MAX = 0.643476;
 items = dir(fullfile(folder_path, '*.mat'));
 filenames = string({items.name});
 
-Process.format_plot("Crazyflie Thrust Versus Distance", "Distance (m)", "Normalized Thrust");
+Process.format_plot("Crazyflie Thrust Gradient", "Distance (m)", "Velocity (m/s)");
 
 for filename = filenames
     load(fullfile(folder_path, filename), 'time', 'forces', 'pos_encoder');
@@ -26,7 +26,7 @@ for filename = filenames
     pos_encoder = pos_encoder - pos_encoder(1) - offset';
 
     if A == 0
-        % scatter(SD, mean(forces.Total(:, 3)) / Config.W / MAX, 5, 'red', 'filled');
+        scatter(SD, 0, 3, mean(forces.Total(:, 3)) / Config.W / MAX, 'filled');
     else
         position_fit = fit_sinusoid(time, pos_encoder, A, F);
         distance = SD + position_fit.A + position_fit(time);
@@ -40,55 +40,16 @@ for filename = filenames
            optional step I considered to remove tangetials, you might want to
         play with this to see which ones to remove without affecting trends
         %}
-        s = scatter(distance(buf:incr:end-buf), forces_smoothed(buf:incr:end-buf), 3, velocity(buf:incr:end-buf), 'filled');
+        s = scatter(distance(buf:incr:end-buf), velocity(buf:incr:end-buf), 3, forces_smoothed(buf:incr:end-buf), 'filled');
         s.MarkerFaceAlpha = 0.25;
     end
 end
 
 colormap(slanCM('jet'));
 a = colorbar;
-a.Label.String = 'Velocity (m/s)';
-
-%% Static
-
-% Load the calibration matrix for the force transducer
-load(['cal_' Config.SENSOR '.mat']);
-
-results = zeros([3 10 2]);
-folders = ["T1", "T2", "T3"];
-
-% Choose data folder
-for f = 1:3
-    folder = folders(f);
-    items = dir(fullfile('Data', '2024_10_31', folder, '*.mat'));
-    filenames = sort({items.name});
-
-    for i = 1:10
-        filename = filenames{i};
-        
-        load(fullfile('Data', '2024_10_31', folder, filename), 'voltages');
-        forces = (cal_mat * voltages')'; % Conversion to forces and moments
-        
-        % Extract and convert parameters
-        parameters = num2cell(sscanf(filename, 'CF%f_SD%f_F%f_A%f.mat'));
-        [~, SD, ~, ~] = deal(parameters{:});
-        SD = SD / 100;
-        
-        Fz = mean(forces(:, 3)) / Config.W;
-        results(f, i, 1) = SD;
-        results(f, i, 2) = Fz;
-    end
-end
-
-% Calculate mean and standard deviation
-est = mean(results, 1);
-err = std(results, 1);
-
-% Plot results (error bars)
-errorbar(est(:, :, 1), est(:, :, 2) / MAX, err(:, :, 2) / MAX, 'ko', 'LineWidth', 1.5);
+a.Label.String = 'Normalized Thrust';
 
 xlim([0 0.26]);
-ylim([0.5 1.05]);
 
 function fitresult = fit_sinusoid(t, s, A, F)
     % Convert to column vectors
