efdef614409de27070be419c9250feb4e7ffc5b7
Alex Wang <alexander_wang@brown.edu>
2025-07-30 17:01:16 -0400

    Updated frequency processing

--- static_processing.m
+++ static_processing.m
@@ -20,20 +20,24 @@ filenames = {items.name};
 SDS = sort(cellfun(@(x) str2double(regexp(x, '(?<=SD)[\d\.]+', 'match')), filenames)) / 100;
 dummy = array2table(zeros(length(SDS), length(folders) + 1), 'VariableNames', ["SD" string(folders)]);
 dummy.SD = SDS';
-results = repmat(table(dummy), 1, 9);
-results.Properties.VariableNames = [Config.NAMES "Current" "Transducer RPS" "Traverse RPS"];
+results = repmat(table(dummy), 1, 8);
+results.Properties.VariableNames = [Config.NAMES "Current" "Transducer RPS"];
 
 % Create progress bar
 fig = uifigure('Name', 'Static Processing');
 d = uiprogressdlg(fig, 'Title', sprintf('Processing (%s)', foldername));
 disp(['Processing <strong>' foldername '</strong>.']);
 
+% Current analysis parameters
+cmin = 1e-3;
+cmax = 5;
+
 % Frequency analysis parameters
 window = hamming(10000);
-nfft = Config.SRATE / 2;
+nfft = 4 * Config.SRATE;
 overlap = 5000;
 fmin = 200;
-fmax = 500;
+fmax = 350;
 
 num_trials = length(folders);
 num_sd = length(SDS);
@@ -60,48 +64,33 @@ for t = 1 : length(folders)
         end
 
         % Mean current
-        results.Current.(trial)(idx) = mean(cf_current);
+        current_mask = cf_current >= cmin & cf_current <= cmax;
+        results.Current.(trial)(idx) = mean(cf_current(current_mask));
 
         % Frequency analysis
-        [P_audio, f_audio] = pwelch(audio, window, overlap, nfft, Config.SRATE);
         [P_force, f_force] = pwelch(forces(:, 3), window, overlap, nfft, Config.SRATE);
-
-        audio_mask = f_audio >= fmin & f_audio <= fmax;
         force_mask = f_force >= fmin & f_force <= fmax;
-        f_audio_trim = f_audio(audio_mask);
-        P_audio_trim = P_audio(audio_mask);
         f_force_trim = f_force(force_mask);
         P_force_trim = P_force(force_mask);
 
-        % Find peaks
-        [pks_audio, locs_audio] = findpeaks(P_audio_trim, f_audio_trim);
-        [pks_force, locs_force] = findpeaks(P_force_trim, f_force_trim);
-
-        locs = locs_audio;
-        pks = pks_audio .* interp1(locs_force, pks_force, locs, 'nearest', 'extrap');
-        [~, top_idx] = maxk(pks, 2);
-        top_locs = locs(top_idx);
-        transducer_rps = max(top_locs);
-        traverse_rps = min(top_locs);
-
+        [~, loc] = max(P_force_trim);
+        transducer_rps = f_force_trim(loc);
         results.("Transducer RPS").(trial)(idx) = transducer_rps;
-        results.("Traverse RPS").(trial)(idx) = traverse_rps;
 
         idx = (t - 1) * num_sd + find(SDS == SD / 100);
         subplot(num_trials, num_sd, idx);
         hold on;
-        plot(f_audio_trim, 10 * log10(P_audio_trim), 'b');
         plot(f_force_trim, 10 * log10(P_force_trim), 'r');
-        scatter(locs_audio, 10 * log10(pks_audio), 8, 'bo', 'filled');
-        scatter(locs_force, 10 * log10(pks_force), 8, 'ro', 'filled');
         xline(transducer_rps, 'g--', 'LineWidth', 1.5);
-        xline(traverse_rps, 'm--', 'LineWidth', 1.5);
         title("T" + t + " SD" + SD);
-        xlim([250 350]);
+        xlim([fmin fmax]);
     end
 end
 
 save(fullfile(folder, 'processed_data.mat'), 'results', '-mat');
+
+set(gcf, 'Renderer', 'painters');
+print(gcf, fullfile(folder, "frequency.svg"), "-dsvg");
 
 d.Value = 1;
 d.Message = 'All files processed';
